fa93bebc158f9197bc7747b74f7cf8bcda3e6e74
KeylimeVI <keylimevi@proton.me>
2025-08-21 19:05:30 -0400

    updated

--- main.go
+++ main.go
@@ -15,67 +15,70 @@ const (
 )
 
 type DiagonalizedArray struct {
-	Array   [][]int
-	i       *int
-	n       int
-	Current *int
-	Next    Direction
-	x       *int
-	y       *int
+	Array [][]int
+	i     int
+	n     int
+	Next  Direction
+	x, y  int
 }
 
 func (array *DiagonalizedArray) Diagonalize() {
+	last := int(math.Sqrt(float64(array.n))) - 1
 
-	*array.Current += *array.i
-	*array.i++
+	for array.i = 1; array.i <= array.n; array.i++ {
+		array.Array[array.y][array.x] = array.i
 
-	if *array.i == array.n {
-		return
-	}
+		if array.i == array.n {
+			return
+		}
 
-	switch array.Next {
-	case Down:
-		*array.y++
-		array.Next = UpRight
-	case Right:
-		*array.x++
-		array.Next = DownLeft
-	case UpRight:
-		*array.y--
-		*array.x++
-		if *array.y == 0 {
-			array.Next = Down
-		} else {
+		switch array.Next {
+		case Down:
+			array.y++
 			array.Next = UpRight
-		}
-	case DownLeft:
-		*array.y++
-		*array.x--
-		if *array.x == 0 {
-			array.Next = Right
-		} else {
+
+		case Right:
+			array.x++
 			array.Next = DownLeft
+
+		case UpRight:
+			if array.x == last {
+				array.y++
+				array.Next = DownLeft
+			} else if array.y == 0 {
+				array.x++
+				array.Next = DownLeft
+			} else {
+				array.x++
+				array.y--
+			}
+
+		case DownLeft:
+			if array.y == last {
+				array.x++
+				array.Next = UpRight
+			} else if array.x == 0 {
+				array.y++
+				array.Next = UpRight
+			} else {
+				array.x--
+				array.y++
+			}
 		}
 	}
-	array.Current = &array.Array[*array.y][*array.x]
-	array.Diagonalize()
-	return
 }
 
 func Diagonal(n int) error {
 	if !isSquare(n) {
 		return fmt.Errorf("Invalid input")
 	}
-	sqrtn := int(math.Floor(math.Sqrt(float64(n))))
+	sqrtn := int(math.Sqrt(float64(n)))
 	arr := make([][]int, sqrtn)
 	for i := range arr {
 		arr[i] = make([]int, sqrtn)
 	}
-	i := new(int)
-	*i = 1
-	x := new(int)
-	y := new(int)
-	d := &DiagonalizedArray{Array: arr, i: i, Next: Down, Current: &arr[0][0], x: x, y: y, n: n}
+
+	d := &DiagonalizedArray{Array: arr, n: n, Next: Down}
 	d.Diagonalize()
 
 	for _, row := range d.Array {
@@ -86,11 +89,9 @@ func Diagonal(n int) error {
 
 func isSquare(n int) bool {
 	sqrt := math.Sqrt(float64(n))
-	if sqrt == float64(int(sqrt)) {
-		return true
-	}
-	return false
+	return sqrt == float64(int(sqrt))
 }
+
 func main() {
-	Diagonal(9)
+	Diagonal(16)
 }
